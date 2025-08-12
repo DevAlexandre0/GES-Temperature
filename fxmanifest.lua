85d1d2953b02d0f3b99ca364cff7e238e52c1370
GESUS <105702323+DevAlexandre0@users.noreply.github.com>
2025-08-12 14:49:24 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,27 +2,31 @@ fx_version 'cerulean'
 game 'gta5'
 
 name 'GES-Temperature'
-description 'Standalone weather and temperature system'
-author 'GESUS'
-version '1.0.0'
+description 'Combined Temperature + Wetness + Stamina modules with SurvCore-aware bridge'
+author 'GES'
+version '1.1.0'
 lua54 'yes'
+
+-- debug: setr ges_debug 1
 shared_scripts {
-    -- '@ox_lib/init.lua',
-    'config.lua'
+  'config/config.temperature.lua',
+  'config/config.wetness.lua',
+  'config/config.stamina.lua',
 }
 
 client_scripts {
-    'client.lua'
+  'client/main.lua',                -- smart bridge (SurvCore-aware)
+  'client/module.temperature.lua',
+  'client/module.wetness.lua',
+  'client/module.stamina.lua',
 }
 
 server_scripts {
-    'server.lua'
+  'server/bridge.lua'               -- optional logging/future hooks
 }
 
 exports {
-    'getTemperatureData'
+  'getTemperatureData',
+  'getWetnessData',
+  'getStaminaData'
 }
-
-server_export 'getServerWeatherData'
-server_export 'setServerTemperature'
-
